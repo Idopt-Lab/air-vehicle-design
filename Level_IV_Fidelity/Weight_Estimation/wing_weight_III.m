9e88934d75788fd42d59ff4d88621fb302b41ded
Casey Chamberlain <caseychamberlain@me.com>
2026-03-31 07:18:13 -0400

    Delete Level_IV_Fidelity directory

--- Level_IV_Fidelity/Weight_Estimation/wing_weight_III.m
+++
@@ -1,9 +0,0 @@
-function [weight] = wing_weight_III(Density, S_ref)
-%WING_WEIGHT_III Summary of this function goes here
-%   Detailed explanation goes here
-
-% Taken from aircraft design metabook, table 
-
-weight = Density * S_ref;
-
-end
