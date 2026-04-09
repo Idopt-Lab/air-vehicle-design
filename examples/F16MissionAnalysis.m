33d45a71072582fea27eabe27d53c766891ecb4e
Casey Chamberlain <caseychamberlain@me.com>
2026-04-09 12:01:47 -0400

    Cut unneeded comments.

--- examples/F16MissionAnalysis.m
+++ examples/F16MissionAnalysis.m
@@ -28,27 +28,6 @@ classdef F16MissionAnalysis < MissionAnalysisModel
                segment_count = length(mission_table.Properties.VariableNames);
                rowcount = length(mission_table.Properties.RowNames);
 
-               % Pre-allocate mission_segments for a nanosecond speed advantage (it's
-               % worth it) (it's not lol)
-               % mission_segments = strings(1, segment_count);
-               % altitudes = zeros(1, segment_count);
-
-               % Acquire segment names
-               % Load segment names into an array or something (IN ORDER)
-               % mission_segments(:) = string(mission_data.Properties.VariableNames(:));
-
-               %% OBTAIN SEGMENT INFO
-               % Extract rows from mission table
-               % Load segment altitudes
-               % altitudes(:) = table2array(mission_data("Altitude (ft)", mission_segments(:)));
-               % payloads(:) = table2array(mission_data("Payload (lbf)", mission_segments(:))); % Payloads (all/relevant)
-               % turns(:) = table2array(mission_data("Turns", mission_segments(:))); % Combat turns (combat)
-               % range(:) = table2array(mission_data("Range (nm)", mission_segments(:))); % Segment range (cruise)
-               % loiter_time(:) = table2array(mission_data("Loiter time (hrs)", mission_segments(:))); % Loitering time (loiter)
-
-               % Cannot do this iteratively without expanding mission_data table to
-               % incorporate new entries, first.
-               % Make a new cell
                for i=1:segment_count % For each segment...
                     alt = mission_table{"Altitude (ft)", i}; % Get the altitude (ft)
                     [Temp, a, P, rho, nu, mu] = atmosisa(alt*0.3048); % Acquire atmospheric data (Kelvin, m/s, Pascals, kg/m^3, m^2/s, kg/(m*s)
