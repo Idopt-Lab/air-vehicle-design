6f54f771edbfa7f953d43b7e313ba580724b7662
Casey Chamberlain <caseychamberlain@me.com>
2026-04-22 10:37:53 -0400

    Updated abstract method inputs.

--- src/ComputationModels/Aerodynamics/AerodynamicsModelLevel2.m
+++ src/ComputationModels/Aerodynamics/AerodynamicsModelLevel2.m
@@ -15,9 +15,9 @@ classdef (Abstract) AerodynamicsModelLevel2 < handle
      methods (Abstract)
           % These should be like wrappers!
           e_osw = get_e_osw(aero_obj, Aircraft, Mission, Requirements)
-          CD0 = get_design_CD0(aero_obj, statevector, geometry_obj, design)
-          CD = get_design_CD(aero_obj, statevector, geometry_obj);
-          DragResults = get_design_drag(aero_obj, statevector, CD0, CL, Cf) % THE MEGA WRAPPER :O
+          CD0 = get_design_CD0(aero_obj, Cf, S_wet_aircraft, S_ref)
+          CD = get_design_CD(aero_obj, CD0, K, CL);
+          DragResults = get_design_drag(aero_obj, geometry_obj, state_input) % THE MEGA WRAPPER :O
           % obj = aircraftname?, aircraft = excel book thing
      end
 end
