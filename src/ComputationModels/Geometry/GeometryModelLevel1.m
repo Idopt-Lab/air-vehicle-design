0c9363fb55658c36b1a85272e3e26e2438f6e461
Casey Chamberlain <caseychamberlain@me.com>
2026-04-23 09:00:20 -0400

    Added Geometry Level 1.

---
+++ src/ComputationModels/Geometry/GeometryModelLevel1.m
@@ -0,0 +1,21 @@
+classdef (Abstract) GeometryModelLevel1 < handle
+     %GEOMETRYMODELLEVEL1 Summary of this class goes here
+     %   Detailed explanation goes here
+
+     properties (Abstract)
+          mainwings
+          HT
+          VT
+          fuselage
+          strakes
+          design
+     end
+
+     methods (Abstract)
+          L_fuselage = get_fus_len(geometry_obj, aircraft_type, W_TO)
+          [HT_AR, HT_lambda, VT_AR, VT_lambda] = est_tail_propers(geometry_obj, aircraft_type)
+          % S_ref_mainwing = get_wing_area(geometry_obj, W_TO) % Figure out
+          % the logistics of this. Update from "Size aircraft" or from
+          % here?
+     end
+end
