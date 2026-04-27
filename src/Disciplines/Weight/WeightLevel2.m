e3319cd12fbb44f2dfe515dabeebbcd4e8b06c95
Casey Chamberlain <caseychamberlain@me.com>
2026-04-27 06:49:27 -0400

    Uses WeightLevel2 model instead of 3.

--- src/Disciplines/Weight/WeightLevel2.m
+++ src/Disciplines/Weight/WeightLevel2.m
@@ -1,4 +1,4 @@
-classdef WeightLevel2 < WeightModelLevel3
+classdef WeightLevel2 < WeightModelLevel2
      %F16WEIGHTESTLEVEL2 Summary of this class goes here
      %   Detailed explanation goes here
      % This is NOT purely component-level.
