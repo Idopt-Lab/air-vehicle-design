0a7f968417e1f9a246d8c54a8634526a9f733aaf
Casey Chamberlain <caseychamberlain@me.com>
2026-05-01 06:30:33 -0400

    Added Stability and Control discipline.

---
+++ src/ComputationModels/StabAndCont/SandCModelLevel3.m
@@ -0,0 +1,16 @@
+classdef (Abstract) SandCModelLevel3 < handle
+     %SANDCMODELLEVEL3 Summary of this class goes here
+     %   Detailed explanation goes here
+
+     properties (Abstract)
+          CG
+          NP
+          C_of_L
+     end
+
+     methods (Abstract)
+          CG = get_CG()
+          NP = get_NP()
+          C_of_L = get_C_of_L()
+     end
+end
