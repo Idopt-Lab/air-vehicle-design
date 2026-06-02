c5e5bd6754697763b8615554d7d404ab1a9e3605
Casey Chamberlain <caseychamberlain@me.com>
2026-06-02 08:58:35 -0400

    Added function for estimating CL_max_w clean.

--- src/Disciplines/Aerodynamics/AeroLevel2.m
+++ src/Disciplines/Aerodynamics/AeroLevel2.m
@@ -6,51 +6,11 @@ classdef AeroLevel2
      % functions) from DATA STORAGE CLASSES (classes that just store data).
      % I think WeaponGenerator2 did that.
 
-     properties
+     properties (Constant)
+          k_lambda = [0.88, 0.95]
      end
 
      methods (Static)
-
-          % % Compute Oswald span efficiency factor
-          % function e_osw = get_e_osw(aero_obj, e_osw)
-          %      % Level 2: Actually compute this?
-          %      aero_obj.e_osw = e_osw;
-          % end
-
-          % Compute K
-          function K = compute_K(e_osw, AR)
-               K = 1/(pi*AR*e_osw);
-          end
-
-          % % Get Cf (should be tabulated by user or the program? Stick with
-          % % user, for now)
-          % function Cf = get_Cf(aero_obj, Cf)
-          %      aero_obj.Cf = Cf;
-          % end
-
-          % % Get CD0
-          % function DragResults = get_design_drag(aero_obj, geometry_obj, state_input)
-          %      W = state_input(4);
-          % 
-          %      % Get q
-          %      q = AeroUtils.compute_q(state_input);
-          % 
-          %      % Get CL
-          %      aero_obj.CL = AeroUtils.compute_CL(W, q, geometry_obj.mainwings.S_ref);
-          % 
-          %      % Get CD0
-          %      DragResults.CD0 = get_design_CD0(aero_obj, aero_obj.Cf, geometry_obj.design.S_wet, geometry_obj.mainwings.S_ref);
-          % 
-          %      % Compute K
-          %      aero_obj.compute_K(aero_obj.e_osw, geometry_obj.mainwings.AR);
-          % 
-          %      % Compute the CD
-          %      DragResults.CD = get_design_CD(aero_obj, DragResults.CD0, aero_obj.K, aero_obj.CL);
-          % 
-          %      % Compute the drag
-          %      DragResults.D = AeroUtils.compute_D(q, DragResults.CD, geometry_obj.mainwings.S_ref);
-          % 
-          % end
 
           % Get CD
           function CD = compute_CD(CD0, K, CL) % Problem: other classes have function with same name. Can I make this private somehow?
@@ -62,14 +22,18 @@ classdef AeroLevel2
                CD0 = Cf * S_wet_aircraft/S_ref;
           end
 
-          % %% FOR MISSION ANALYSIS
-          % % Compute L/D (using revised method) (I should probably store
-          % % mission segment results somewhere...)
-          % function [LD_ratio] = compute_revised_LD_ratio(W, q, S, CD0, e, AR)
-          %      CL = 2*W/(q*S);
-          %      K = 1/(pi*e*AR);
-          %      LD_ratio = CL/(CD0 + K * CL^2);
-          % end
+          % Estimate CL_max_w (clean)
+          % Source: Airplane Design Vol 2, Roskam, eq 7.3
+          function output = CL_max_w(cl_max_r, cl_max_t)
+               if (0.4 < lambda) && (lambda <= 1.0)
+                 % use k_lambda = 0.88 (k_lambda(1))
+                 output = k_lambda(1)*(cl_max_r + cl_max_t)^(2);
+               elseif (0.0 < lambda) && (lambda <= 0.4)
+                    output = k_lambda(2)*(cl_max_r + cl_max_t)^(2);
+               else
+                    error("Error handler.")
+               end
+          end
 
      end
 
--- src/Disciplines/Subsystems/SubsystemsLevel2.m
+++ src/Disciplines/Subsystems/SubsystemsLevel2.m
@@ -11,7 +11,9 @@ classdef SubsystemsLevel2
           function output = wingfuelvolume(S_ref, b, tc_root, tc_tip, lambda_w)
                % Estimates the wing fuel volume for prliminary design.
                % Source: Roskam, Airplane Design Vol 2, eq 6.2
-               % Statistical estimation, so probably belongs in L2 
+               % Statistical estimation, so probably belongs in L2
+               % Accounts for required dry bays + lightning strike problem
+               % :)
                tau_w = tc_tip/tc_root;
                output = 0.54*(S_ref^2/b)*tc_root*((1+lambda_w*(tau_w)^(1/2) + lambda_w^2*tau_w)/(1+lambda_w^2));
           end
