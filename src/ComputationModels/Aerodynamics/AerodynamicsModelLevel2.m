ce0dc7ba3f7f43cd01b8c42d62136226d09983fa
Casey Chamberlain <caseychamberlain@me.com>
2026-06-03 07:00:43 -0400

    Cleanup

--- src/ComputationModels/Aerodynamics/AerodynamicsModelLevel2.m
+++ src/ComputationModels/Aerodynamics/AerodynamicsModelLevel2.m
@@ -29,7 +29,6 @@ classdef (Abstract) AerodynamicsModelLevel2 < handle
 
 
      methods (Abstract)
-          % These should be like wrappers!
           e_osw = get_e_osw(AR, Lambda_LE)
           LD_max = get_LD_max(AR, e_osw, CD0)
           % AR_wet = get_AR_wet(b, S_wet)
@@ -41,7 +40,7 @@ classdef (Abstract) AerodynamicsModelLevel2 < handle
           CDi = get_CDi(statevector, CL, e_osw, AR)
           Delta_CD0 = get_Delta_CD0(configuration, rangeMode) % This should get you the Delta_CD0 values you need. (use Raymer 12.61
           CL_minD = get_CL_minD(airfoil_type, CL_min, CD0)
-          Cf = get_Cf(aircraft_type, n_engines)
+          Cf = get_Cf(aircraft_type, n_engines) % Using L1 until a suitable replacement is found.
           CL_max = get_CL_max_values(aircraft_type, config, rangeMode) % This should get you the CL_max values you need (CL_max_TO, CL_max_Landing, etc)
           Delta_CL_max = get_Delta_CL_max_values(CL_max_dirty, CL_max_clean, isTakeoffOrLanding) % This should be able to get you the Delta_CL_max values you need.
           Delta_Cl_max = get_Delta_Cl_max_values(liftdevice, config, cp_c) % this should get you the values you need (Delta_Cl_max_TO, Delta_Cl_max_L)
